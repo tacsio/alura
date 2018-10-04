698362c553628043b1362b65c0061339a0574552
Tarcisio Coutinho <tcs5cin@gmail.com>
2018-10-04 20:15:19 -0300

    js es6

--- js-es6/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ js-es6/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -11,9 +11,25 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
+
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map((val, index) => {
+                return val - index % 2
+            }));
+
+        data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map((val, index) => val - index % 2));
         
-        console.log(this._inputData.value);
-        console.log(this._inputQuantidade.value);
-        console.log(this._inputValor.value);
+        let negociacao = new Negociacao(
+            new Date(this._inputData.value.replace('-', ',')),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+
+        console.log(negociacao);
     }
 }
