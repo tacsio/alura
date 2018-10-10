a3ce1e6a4ecf888b9c1b79797ba2fa0443e00f2c
Tarcisio Coutinho <tcs5cin@gmail.com>
2018-10-10 13:53:52 -0300

    Adiciona lista de negociacoes

--- js-es6/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ js-es6/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,20 +7,29 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
-        
-        let data =  DateHelper.textoParaData(this._inputData.value);
-        
-        let negociacao = new Negociacao(
-            data,
+        this._listaNegociacoes.adiciona(this._criarNegociacao());
+        this._limpaFormulario();
+
+        console.log(this._listaNegociacoes);
+    }
+
+    _criarNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
+    }
 
-        let textoData = DateHelper.dataParaTexto(negociacao.data);
-        console.log(textoData);
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();   
     }
 }
