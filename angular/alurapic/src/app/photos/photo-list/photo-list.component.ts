b1dc6bf4bbcba6c96b17edb1a4df3e8eacdafbfd
Tarcisio Coutinho <tcs5cin@gmail.com>
2018-10-16 16:27:54 -0300

    update

--- angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Photo } from '../photo/photo';
@@ -9,22 +9,43 @@ import { PhotoService } from './../photo/photo.service';
 	templateUrl: './photo-list.component.html',
 	styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnChanges {
 
-	photos:Photo[] = []
+	allPhotos: Photo[] = []
+	photos: Photo[] = []
+	filter: string = ''
 	
 	constructor(private photoService: PhotoService,
 				private activatedRoute: ActivatedRoute) { }
-
+				
 	ngOnInit():void {
 		//recupera path param
 		const username = this.activatedRoute.snapshot.params.username;
 
 		this.photoService.listFromUser(username)
 			.subscribe(
-				photos => this.photos = photos,
+				photos => { 
+					this.allPhotos = photos;
+					this.filterPhotos();
+				},
 				err => console.log(err)
 			);
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		console.log('entra')
+		if(changes.filter) {
+			this.photos = this.filterPhotos();
+		} 
+	}
+
+	filterPhotos(): any {
+		if(this.filter.length > 2){
+			let regex: RegExp = new RegExp(`*${this.filter}*`);			
+			this.photos = this.allPhotos.filter(photo => regex.test(photo.description));
+		} else {
+			this.photos = this.allPhotos;
+		}
 	}
 
 }
